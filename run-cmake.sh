bcc3064076653272366ea83f8bd126a37720a5d3
Mark Burton <markb@smartavionics.com>
2019-09-16 22:56:33 +0100

    Combine linux and osx versions.

--- run-cmake.sh
+++ run-cmake.sh
@@ -1,4 +1,17 @@
 #! /bin/sh
 
-PATH=/opt/cura/bin/:$PATH cmake -DCMAKE_PREFIX_PATH=~/cura/local -DCMAKE_INSTALL_PREFIX=~/cura/local ..
+if [[ $(uname) == "Darwin" ]]; then
+  export CURA_TARGET_OSX_VERSION=10.13
+  . ../env_osx.sh
+  if [[ -e /Library/Developer/CommandLineTools/usr/bin/clang ]]; then
+    export CC="/Library/Developer/CommandLineTools/usr/bin/clang"
+    export CXX="/Library/Developer/CommandLineTools/usr/bin/clang++"
+  fi
+  export CURA_HOME=~/cura/local
+  mkdir $CURA_HOME
+else
+  export CURA_HOME=/opt/cura
+fi
+
+PATH=$CURA_HOME/bin/:$PATH cmake -DCMAKE_PREFIX_PATH=$CURA_HOME -DCMAKE_INSTALL_PREFIX=$CURA_HOME ..
 
