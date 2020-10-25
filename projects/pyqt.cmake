5d22f1f479759df00b8a134d9028cd34e31222dc
Mark Burton <burtoogle@gmail.com>
2020-10-25 17:46:37 +0000

    Now using sip 4.19.24 as 4.19.19 is no longer available.

--- projects/pyqt.cmake
+++ projects/pyqt.cmake
@@ -3,7 +3,7 @@ if(BUILD_OS_WINDOWS)
     # Since PyQt5 5.11, PyQt5 requires its standalone sip module which needs to be built with the option
     # --sip-module PyQt5.sip. See https://www.riverbankcomputing.com/static/Docs/PyQt5/installation.html
     ExternalProject_Add(PyQtSip
-        URL https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz
+        URL https://www.riverbankcomputing.com/static/Downloads/sip/4.19.24/sip-4.19.24.tar.gz
         #URL_MD5 236578d2199da630ae1251671b9a7bfe
         CONFIGURE_COMMAND ${Python3_EXECUTABLE} configure.py --platform win32-msvc2015 --sip-module PyQt5.sip
         BUILD_IN_SOURCE 1
@@ -41,7 +41,7 @@ else()
     # Since PyQt5 5.11, PyQt5 requires its standalone sip module which needs to be built with the option
     # --sip-module PyQt5.sip. See https://www.riverbankcomputing.com/static/Docs/PyQt5/installation.html
     ExternalProject_Add(PyQtSip
-        URL https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz
+        URL https://www.riverbankcomputing.com/static/Downloads/sip/4.19.24/sip-4.19.24.tar.gz
         #URL_MD5 236578d2199da630ae1251671b9a7bfe
         CONFIGURE_COMMAND ${Python3_EXECUTABLE} configure.py --sip-module PyQt5.sip
         BUILD_IN_SOURCE 1
--- projects/sip.cmake
+++ projects/sip.cmake
@@ -19,7 +19,7 @@ else()
 endif()
 
 ExternalProject_Add(Sip
-    URL https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz
+    URL https://www.riverbankcomputing.com/static/Downloads/sip/4.19.24/sip-4.19.24.tar.gz
     #URL_MD5 236578d2199da630ae1251671b9a7bfe
     CONFIGURE_COMMAND ${sip_command}
     BUILD_IN_SOURCE 1
