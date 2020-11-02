ba5d44d7440970ba400cbf511b8beac2dc58bec5
Mark Burton <markb@smartavionics.com>
2020-11-02 09:46:26 +0000

    Add custom command to install sip.h and sip5 module.

--- projects/sip.cmake
+++ projects/sip.cmake
@@ -26,8 +26,16 @@
 #)
 
 add_custom_target(Sip
-    COMMAND ${Python3_EXECUTABLE} -m pip install sip==5.4.0
     COMMENT "Installing sip"
+    DEPENDS ${CMAKE_PREFIX_PATH}/include/python3.5/sip.h
+)
+
+add_custom_command(
+    OUTPUT ${CMAKE_PREFIX_PATH}/include/python3.5/sip.h
+    COMMAND ${Python3_EXECUTABLE} -m pip install sip==5.4.0
+    COMMAND ${CMAKE_PREFIX_PATH}/bin/sip-module --sdist --target-dir ${CMAKE_PREFIX_PATH}/lib/python3.5 sip5
+    COMMAND ${Python3_EXECUTABLE} -m pip install ${CMAKE_PREFIX_PATH}/lib/python3.5/sip5-12.8.1.tar.gz
+    COMMAND ${CMAKE_PREFIX_PATH}/bin/sip-module --sip-h --target-dir ${CMAKE_PREFIX_PATH}/include/python3.5 sip5
 )
 
 SetProjectDependencies(TARGET Sip DEPENDS Python)
