99a8c010acfe8fab7cbf473c239ef046054b2cce
Mark Burton <burtoogle@gmail.com>
2020-11-27 08:37:39 +0000

    Change sip dependencies (again).

--- projects/sip.cmake
+++ projects/sip.cmake
@@ -27,15 +27,15 @@
 
 add_custom_target(Sip
     COMMENT "Installing sip"
-    DEPENDS ${CMAKE_PREFIX_PATH}/bin/sip-module ${CMAKE_PREFIX_PATH}/include/python3.5/sip.h
+    DEPENDS ${CMAKE_PREFIX_PATH}/include/python3.5/sip.h
 )
 
 add_custom_command(
-    OUTPUT ${CMAKE_PREFIX_PATH}/bin/sip-module ${CMAKE_PREFIX_PATH}/include/python3.5/sip.h
-    COMMAND ${Python3_EXECUTABLE} -m pip install --force-reinstall sip==5.4.0
-    COMMAND ${CMAKE_PREFIX_PATH}/bin/sip-module --sdist --target-dir ${CMAKE_PREFIX_PATH}/lib/python3.5 sip5
+    OUTPUT ${CMAKE_PREFIX_PATH}/include/python3.5/sip.h
+    COMMAND ${Python3_EXECUTABLE} -m pip install sip==5.4.0
+    COMMAND ${Python3_EXECUTABLE} ${CMAKE_PREFIX_PATH}/bin/sip-module --sdist --target-dir ${CMAKE_PREFIX_PATH}/lib/python3.5 sip5
     COMMAND ${Python3_EXECUTABLE} -m pip install ${CMAKE_PREFIX_PATH}/lib/python3.5/sip5-12.8.1.tar.gz
-    COMMAND ${CMAKE_PREFIX_PATH}/bin/sip-module --sip-h --target-dir ${CMAKE_PREFIX_PATH}/include/python3.5 sip5
+    COMMAND ${Python3_EXECUTABLE} ${CMAKE_PREFIX_PATH}/bin/sip-module --sip-h --target-dir ${CMAKE_PREFIX_PATH}/include/python3.5 sip5
 )
 
 SetProjectDependencies(TARGET Sip DEPENDS Python)
